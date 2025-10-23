a1355a649dce1913ba6265e0aae09d828a9153ca
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-10-23 04:21:39 -0700

    fix webp detection

--- Program.cs
+++ Program.cs
@@ -145,7 +145,7 @@ public static class SwarmAPI
             List<(byte[], string)> images = [.. generated["images"].Select(img =>
             {
                 string type = img.ToString().After("data:").Before(";");
-                string ext = type == "image/gif" ? "gif" : (type == "video/webp" ? "webp" : "jpg");
+                string ext = type == "image/gif" ? "gif" : (type == "video/webp" || type == "image/webp" ? "webp" : "jpg");
                 Console.WriteLine($"Generated image of mime-type {type}, interpreting as {ext}");
                 byte[] data = Convert.FromBase64String(img.ToString().After(";base64,"));
                 return (data, ext);
